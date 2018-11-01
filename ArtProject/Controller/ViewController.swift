5e30586163839d5b857b83bd84269a89c8fd7142
rphi8796 <jlkfop@gmail.com>
2018-11-01 11:53:04 -0600

    I created the project

---
+++ ArtProject/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArtProject
+//
+//  Created by Phillips, Ryan on 11/1/18.
+//  Copyright © 2018 Phillips, Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
